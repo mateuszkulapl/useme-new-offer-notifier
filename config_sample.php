7c63ac216da5f471aa0df57683ac5119fbfd8519
mateuszkulapl <mateuszkulapl@gmail.com>
2022-04-28 19:16:11 +0200

    configuration

---
+++ config_sample.php
@@ -0,0 +1,11 @@
+<?php
+//set variables then rename file to config.php
+$host = 'localhost';
+$db   = 'database_name';
+$user = 'database_user';
+$pass = 'database_password';
+$port = "database_port";
+$charset = 'utf8mb4';
+
+$email_sender='';
+$email_recipient='';
